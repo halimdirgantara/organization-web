a2c09267a2be58e55504682021636ad3aecbbddb
halimdirgantara <57507252+halimdirgantara@users.noreply.github.com>
2023-05-10 08:24:04 +0700

    fix: wire ui scripts

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,8 +12,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
         <wireui:scripts />
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         <script src="//unpkg.com/alpinejs" defer></script>
     </head>
     <body>
