e6a107401ec737ef4c06c5b9f17147950ceb97c9
halimdirgantara <halimdirgantara@gmail.com>
2023-06-02 16:39:49 +0700

    NOT FINSH! fix: header and remove header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,18 +25,12 @@
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
         @livewire('navigation-menu')
         @include('sidebar-menu')
-        <!-- Page Heading -->
-        @if (isset($header))
-            <header class="overflow-auto bg-white dark:bg-gray-800 shadow relative z-10">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endif
+
 
         <!-- Page Content -->
         <main>
             <div class="md:ml-64">
+                <!-- Page Heading -->
                 {{ $slot }}
             </div>
         </main>
