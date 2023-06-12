395663117b2cef2fa2127e367ac45e349acabd9c
halimdirgantara <halimdirgantara@gmail.com>
2023-06-12 20:05:49 +0700

    fix: navbar organization title

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,6 +1,6 @@
 <nav x-data="{ open: false }" class="fixed top-0 left-0 z-50 w-full bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
-    <div class=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
@@ -11,7 +11,7 @@
 
                 </div> --}}
                 <!-- Org -->
-                <div class="shrink-0 flex items-center">
+                <div class="shrink-0 flex items-center text-left">
                     <div class="text-lg text-primary-500 text font-bold">
                         {{ Auth::user()->organization->name }}
                     </div>
